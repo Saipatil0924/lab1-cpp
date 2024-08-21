20340b988ab23478cde24b1fe371cdbd5d0395e0
Saipatil0924 <aspatil1971@gmail.com>
2024-08-21 11:05:56 +0530

    Add files via upload

---
+++ sai.py
@@ -0,0 +1,51 @@
+def martix_1():
+     r=int(input("enter the no of row you want "))
+     c=int(input("enter the no of column you want "))
+     m=[]
+     e=int(input("enter tne entriesrow wise"))
+     for i in range(0,r):
+          a=[]
+          for j in range(0,c):
+               a.append(int(input("enter the no")))
+          m.append(a)
+                        
+     for i in range(0,r):
+     for j in range(0,c):
+          print(m[i][j],end=" ")
+     print()
+     
+     
+
+     
+     
+def martix_2():
+     r=int(input("enter the no of row you want "))
+     c=int(input("enter the no of column you want "))
+     m=[]
+     e=int(input("enter tne entriesrow wise"))
+     for i in range(0,r):
+          a=[]
+          for j in range(0,c):
+               a.append(int(input("enter the no")))
+          m.append(a)
+                        
+     for i in range(0,r):
+     for j in range(0,c):
+          print(m[i][j],end=" ")
+     print()
+
+
+
+while True:
+     print("____________________________________-operation-__________________________________________________________")
+     print("____________________________________-ch=1 input of given tow matrix you want to add-_____________________ ")
+     
+     ch=int(input("enter the chouice fromthe above menue: "))
+     for i in range(0,ch):
+               if ch==1 :
+                     
+     
+
+
+
+
